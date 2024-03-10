875ba886437ff8a152d581e3c90b93e67aa01324
MeekhalMathew <147236828+MeekhalMathew@users.noreply.github.com>
2024-03-09 20:04:46 -0800

    Random quote generator was set up.

---
+++ main.js
@@ -0,0 +1,16 @@
+let quote = document.getElementById('quote');
+let author = document.getElementById('author');
+let btn = document.getElementById('btn');
+let quoteContainer = document.querySelector('.quote-container'); 
+
+btn.addEventListener('click', handleClick);
+
+function handleClick() {
+  fetch('https://dummyjson.com/quotes/random')
+    .then(data => data.json())
+    .then(output => {
+      quote.innerHTML = output.quote;
+      author.innerHTML = output.author;
+      quoteContainer.style.display = 'block'; 
+    });
+}
